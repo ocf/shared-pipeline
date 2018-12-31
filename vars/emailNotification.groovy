81bd66984bae46066603caac68f02bd80295346b
Jason Perrin <jvperrin@ocf.berkeley.edu>
2018-12-31 13:31:21 -0800

    Add IRC formatting to build notifications

--- vars/emailNotification.groovy
+++ vars/emailNotification.groovy
@@ -1,9 +1,8 @@
 /** Send an email notification when a build fails */
 
 def call(String email = 'root@ocf.berkeley.edu') {
-    result = currentBuild.result ?: 'SUCCESS'
     subject = "${JOB_NAME} - Build #${BUILD_NUMBER} - Failure!"
-    message = "Project ${JOB_NAME} (#${BUILD_NUMBER}): ${result}: ${BUILD_URL}"
+    message = "Project ${JOB_NAME} (#${BUILD_NUMBER}): ${currentBuild.currentResult}: ${BUILD_URL}"
 
     if (BRANCH_NAME == 'master') {
         mail to: email, subject: subject, body: message
--- vars/ircNotification.groovy
+++ vars/ircNotification.groovy
@@ -9,8 +9,17 @@ def call(String channel = '#rebuild-spam',
          String server = 'irc.ocf.berkeley.edu:6697') {
     cleanJobName = JOB_NAME.replaceAll('[^a-zA-Z0-9_-]', '-')
     nick = "${basenick}-${cleanJobName}".take(32)
-    result = currentBuild.result ?: 'SUCCESS'
-    message = "Project ${JOB_NAME} (#${BUILD_NUMBER}): ${result}: ${BUILD_URL}"
+    result = currentBuild.currentResult
+
+    if (result == 'SUCCESS') {
+        // Display success messages in green text on IRC
+        resultDisplay = "\u000303${result}\u000f"
+    } else {
+        // Display failure messages in red bold text on IRC and bold text on Slack
+        resultDisplay = "\u000304\u0002*${result}*\u000f"
+    }
+
+    message = "Project ${JOB_NAME} (#${BUILD_NUMBER}): ${resultDisplay}: ${BUILD_URL}"
     sh """
         (
         echo NICK ${nick}
