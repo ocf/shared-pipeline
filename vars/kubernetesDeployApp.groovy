3d65cfe6e20f5d7263fa06697c78a427844277af
fydai <fydai@ocf.berkeley.edu>
2019-05-30 13:47:00 -0700

    Removed ./kubernetes argument for ocf-kubernetes-deploy

--- vars/kubernetesDeployApp.groovy
+++ vars/kubernetesDeployApp.groovy
@@ -10,7 +10,7 @@ def call(String app, String version) {
                         serverUrl: 'https://kubernetes.ocf.berkeley.edu:6443'
         ]) {
             sh """
-                ocf-kubernetes-deploy "$app" "$version" ./kubernetes
+                ocf-kubernetes-deploy "$app" "$version"
             """
         }
     }
